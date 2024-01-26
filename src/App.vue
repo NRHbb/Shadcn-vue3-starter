567e68b8432c9b7391578ffcc95f5d853667c2a8
NR Habibi <nrhbb14@gmail.com>
2024-01-26 10:10:43 +0800

    fix some code and styling

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <h1>Welcome SHADCN vue3 starter pack</h1>
+  <h1 class="text-3xl font-bold flex justify-center items-center h-screen">Welcome SHADCN vue3 starter pack</h1>
   <RouterView />
 </template>
 
