8b79996ca71aafcb4ad6d68680c7b4a95a1b6efb
NR Habibi <nrhbb14@gmail.com>
2024-01-26 10:06:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <h1>Welcome SHADCN vue3 starter pack</h1>
+  <RouterView />
+</template>
+
+
